fa1a383119e397946d813d700a8a80fea0197ed3
ShayDaise <102685776+ShayDaise@users.noreply.github.com>
2022-05-21 19:27:26 -0400

    added basic game functionality

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,7 +3,7 @@ var playerHealth = 100;
 var playerAttack = 10;
 
 // You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
+console.log(playerName,`${playerName} has ${playerAttack} attack power and ${playerHealth} health` );
 
 var enemyName = "Roborto";
 var enemyHealth = 50;
@@ -11,6 +11,32 @@ var enemyAttack = 12;
 
 var fight = function() {
   window.alert("Welcome to Robot Gladiators!");
+  
+  enemyHealth = enemyHealth - playerAttack
+  
+  console.log(`${playerName} attacked ${enemyName}. ${enemyName} now has ${enemyHealth} health remaining.`)
+  
+  playerHealth = playerHealth - enemyAttack
+
+  console.log(`${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} health remaining.`);
+
 };
 
 fight();
+
+if (enemyHealth <= 0) {
+  window.alert(enemyName + " has died!")
+} 
+else {
+  window.alert(enemyName + " still has " + enemyHealth + " health left.");
+};
+
+if (playerHealth <= 0) {
+  window.alert(playerName + " has died!");
+} 
+else {
+  window.alert(playerName + " still has " + playerHealth + " health left.");
+};
+
+x
+
